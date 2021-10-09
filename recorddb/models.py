793af7424dfc48ac342d7e9af02b82c97457b12b
Hydrohs <hydrohs@hydrohs.com>
2021-10-09 15:43:44 -0300

    Add admin user

--- recorddb/models.py
+++ recorddb/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Record(models.Model):
-
     class Condition(models.TextChoices):
         NEW = 'N'
         USED = 'U'
@@ -10,3 +9,6 @@ class Record(models.Model):
     artist = models.CharField(max_length=200)
     owned = models.BooleanField
     condition = models.CharField(max_length=1, choices=Condition.choices, default=Condition.NEW)
+
+    def __str__(self):
+        return self.title
