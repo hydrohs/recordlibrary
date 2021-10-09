526a70cd53d4c0b0b303006c7ace7bb16b340f07
Hydrohs <hydrohs@hydrohs.com>
2021-10-09 16:04:51 -0300

    Fix class/db

--- recorddb/models.py
+++ recorddb/models.py
@@ -2,13 +2,14 @@ from django.db import models
 
 class Record(models.Model):
     class Condition(models.TextChoices):
+        NONE = '', 'N/A'
         NEW = 'N'
         USED = 'U'
     
     title = models.CharField(max_length=200)
     artist = models.CharField(max_length=200)
-    owned = models.BooleanField
-    condition = models.CharField(max_length=1, choices=Condition.choices, default=Condition.NEW)
+    owned = models.BooleanField(default=False)
+    condition = models.CharField(max_length=1, blank=True, choices=Condition.choices, default=Condition.NONE)
 
     def __str__(self):
         return self.title
