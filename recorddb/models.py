011052774ff5728ed93404c381f2d32b8e243e50
Hydrohs <hydrohs@hydrohs.com>
2021-10-09 19:53:43 -0300

    Add artist class

--- recorddb/models.py
+++ recorddb/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+
+class Artist(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 class Record(models.Model):
     class Condition(models.TextChoices):
@@ -7,7 +14,7 @@ class Record(models.Model):
         USED = 'U'
     
     title = models.CharField(max_length=200)
-    artist = models.CharField(max_length=200)
+    artist = models.ForeignKey(Artist, on_delete=CASCADE)
     owned = models.BooleanField(default=False)
     condition = models.CharField(max_length=1, blank=True, choices=Condition.choices, default=Condition.NONE)
 
