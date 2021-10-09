7fa297da17ca7561f07e348db74186abfa63c0be
Hydrohs <hydrohs@hydrohs.com>
2021-10-09 14:55:43 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Vinyl record management program using Django so I can tell what records I have/want when shopping
---
+++ recorddb/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RecorddbConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'recorddb'
